a487c4cbc464cb6bc3f9ab310101596dc8aa130b
aly-farouz22 <aly.farouz@ucalgary.ca>
2025-03-05 11:27:21 -0700

    Task.java Done

--- src/edu/ucalgary/oop/Task.java
+++ src/edu/ucalgary/oop/Task.java
@@ -2,7 +2,34 @@ package edu.ucalgary.oop;
 
 import java.util.Objects;
 
-public class Task {
+public class Task implements Cloneable {
+    private int id;
+    private String title;
+    private boolean isCompleted;
+
+    public int getID() {
+        return id;
+    }
+    public void setID(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title ;
+    }
+    public void setTitile(String title) {
+        this.title = title;
+    }
+    public boolean getIsCompleted() {
+        return isCompleted ;
+    }
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }    
+
+    public Object clone() throws CloneNotSupportedException {
+        Task copy = (Task)super.clone();
+        return copy;
+    }
 
     @Override
     public boolean equals(Object obj) {
