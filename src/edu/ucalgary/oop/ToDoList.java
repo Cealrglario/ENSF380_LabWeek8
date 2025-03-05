d6f74f4c9f955089bcdfafb3cec2519b27231d92
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2025-03-05 11:43:35 -0700

    Implement interface in ToDoList

--- src/edu/ucalgary/oop/ToDoList.java
+++ src/edu/ucalgary/oop/ToDoList.java
@@ -4,9 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class ToDoList {
-    private List<Task> tasks = new ArrayList<>() ;
-    private Stack<List<Task>>
+public class ToDoList implements IToDoList {
+    private List<Task> tasks = new ArrayList<>();
+    private Stack<List<Task>> taskHistory = new Stack<>();
 
 
+    @Override
+    public void add(Task task) {
+
+    }
+
+    @Override
+    public void complete(Task task) {
+
+    }
+
+    @Override
+    public void delete(Task task) {
+
+    }
+
+    @Override
+    public void edit(Task task) {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+
+    @Override
+    public ArrayList<Task> list() {
+        return null;
+    }
 }
