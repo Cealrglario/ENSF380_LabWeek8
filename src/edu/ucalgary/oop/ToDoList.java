96298d53d999d655a94e8c4a76dee24d4894f229
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2025-03-05 13:04:39 -0700

    Fixed undo method/backup logic

--- src/edu/ucalgary/oop/ToDoList.java
+++ src/edu/ucalgary/oop/ToDoList.java
@@ -8,6 +8,10 @@ public class ToDoList implements IToDoList {
     private ArrayList<Task> tasks;
     private Stack<ArrayList<Task>> taskHistory;
 
+    public void backupToDoList() {
+        this.taskHistory.add(new ArrayList<>(tasks)); // We need to backup a COPY of tasks, not a reference to it
+    }
+
     public ToDoList() {
         this.tasks = new ArrayList<>();
         this.taskHistory = new Stack<>();
@@ -15,25 +19,25 @@ public class ToDoList implements IToDoList {
 
     @Override
     public void addTask(Task task) {
-        this.taskHistory.add(tasks);
+        backupToDoList();
         this.tasks.add(task);
     }
 
     @Override
     public void completeTask(String task) {
-        this.taskHistory.add(tasks);
+        backupToDoList();
         this.tasks.get(Integer.parseInt(task) - 1).setIsCompleted(true);
     }
 
     @Override
     public void deleteTask(String task) {
-        this.taskHistory.add(tasks);
+        backupToDoList();
         this.tasks.remove(Integer.parseInt(task) - 1);
     }
 
     @Override
     public void editTask(String task, String title, boolean isCompleted) {
-        this.taskHistory.add(tasks);
+        backupToDoList();
         this.tasks.get(Integer.parseInt(task) - 1).setTitle(title);
         this.tasks.get(Integer.parseInt(task) - 1).setIsCompleted(isCompleted);
         this.tasks.get(Integer.parseInt(task) - 1).setID(task);
